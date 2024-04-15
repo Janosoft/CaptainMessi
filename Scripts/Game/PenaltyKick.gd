5c2ddd91860017859355caec6d560f98c4319d36
Alejandro Lodes <janosoft@gmail.com>
2024-04-15 20:37:04 -0300

    penaltyKick

---
+++ Scripts/Game/PenaltyKick.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var inter_miami_cf_2023 = $InterMiamiCf2023
+
+func _ready():
+	inter_miami_cf_2023.showPlayer("1 - Callender","defaultMini")
---
+++ Scripts/Game/UI/UI.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export_flags("Relator:1", "Time:2", "Teams:4", "Pk:8", "Balloon:16", "Stats:32", "Actions:64") var elements = 0
+@onready var relator = $Relator
+@onready var time = $Time
+@onready var teams = $Teams
+@onready var balloon = $Balloon
+@onready var pk = $Pk
+@onready var stats = $Stats
+@onready var actions = $Actions
+
+func _ready():
+	if elements & 1: relator.visible= true
+	if elements & 2: time.visible= true
+	if elements & 4: teams.visible= true
+	if elements & 8: pk.visible= true
+	if elements & 16: balloon.visible= true
+	if elements & 32: stats.visible= true
+	if elements & 64: actions.visible= true
