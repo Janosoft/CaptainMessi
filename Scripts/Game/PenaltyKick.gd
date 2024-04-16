1f2c7d48c4ce97ed945e81c193ebac764d51d531
Alejandro Lodes <janosoft@gmail.com>
2024-04-15 23:25:29 -0300

    starting penalties

--- Scripts/Game/PenaltyKick.gd
+++ Scripts/Game/PenaltyKick.gd
@@ -1,6 +1,13 @@
 extends Node2D
-
-@onready var inter_miami_cf_2023 = $InterMiamiCf2023
+@export_file("*.tscn") var team1File
+@export_file("*.tscn") var team2File
+@onready var team1 = $Team1
+@onready var team2 = $Team2
 
 func _ready():
-	inter_miami_cf_2023.showPlayer("1 - Callender","defaultMini")
+	team1.replace_by(load(team1File).instantiate())
+	team2.replace_by(load(team2File).instantiate())
+	print_debug(team1.name)
+	#team1.getGoalkeeperName()
+	#team1.showPlayer(firstTeam.getGoalkeeperName(), "defaultMini")	
+	#inter_miami_cf_2023.showPlayer("1 - Callender","defaultMini")
---
+++ Scripts/Teams/Cruz Azul 2023.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+const SHIRT_1: Color = Color(0.008, 0.310, 0.918, 1.0)
+const SHIRT_2: Color = Color(0.623, 0.055, 0.086, 1.0)
+const PANTS: Color = Color(0.0, 0.184, 0.811, 1.0)
+const SOCKS_1: Color = Color(0.035, 0.286, 1.0, 1.0)
+const SOCKS_2: Color = Color(0.020, 0.169, 0.812, 1.0)
+const KEEPER_1: Color = Color(0.521, 0.141, 0.663, 1.0)
+const KEEPER_2: Color = Color(0.380, 0.055, 0.565, 1.0)
+
+@onready var players = $Players
+@onready var goalkeepers = $Goalkeepers
+
+func _ready():
+#region Set Players Colors
+	for player in players.get_children():
+		player.material= ShaderMaterial.new()
+		player.material.shader= preload("res://Shaders/player.gdshader")
+		player.material.set_shader_parameter('Shirt_1', SHIRT_1)
+		player.material.set_shader_parameter('Shirt_2', SHIRT_2)
+		player.material.set_shader_parameter('Pants', PANTS)
+		player.material.set_shader_parameter('Socks_1', SOCKS_1)
+		player.material.set_shader_parameter('Socks_2', SOCKS_2)
+		player.material.set_shader_parameter('Skin', player.skinColor)
+		player.material.set_shader_parameter('Hair', player.hairColor)
+#endregion
+#region Set Goalkeepers Colors
+	for goalkeeper in goalkeepers.get_children():
+		goalkeeper.material= ShaderMaterial.new()
+		goalkeeper.material.shader= preload("res://Shaders/goalkeeper.gdshader")
+		goalkeeper.material.set_shader_parameter('Shirt_1', KEEPER_1)
+		goalkeeper.material.set_shader_parameter('Shirt_2', KEEPER_2)
+		goalkeeper.material.set_shader_parameter('Skin', goalkeeper.skinColor)
+		goalkeeper.material.set_shader_parameter('Hair', goalkeeper.hairColor)
+#endregion
+	
+func showPlayer(playerName:String, animation:String):
+	for player:CharacterBody2D in players.get_children():
+		if (player.name == playerName):
+			player.setAnimation(animation)
+			player.visible = true
+		else:
+			player.visible = false
+	for goalkeeper:CharacterBody2D in goalkeepers.get_children():
+		if (goalkeeper.name == playerName):
+			goalkeeper.setAnimation(animation)
+			goalkeeper.visible = true;
+		else:
+			goalkeeper.visible = false
+
+func getGoalkeeperName():
+	return goalkeepers.get_child(0).name
