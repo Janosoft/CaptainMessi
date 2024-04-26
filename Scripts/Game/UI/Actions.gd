c7ff227c0eedd7ac2926c21b8984d54b4673d956
Alejandro Lodes <janosoft@gmail.com>
2024-04-26 17:01:19 -0300

    animation player

--- Scripts/Game/UI/Actions.gd
+++ Scripts/Game/UI/Actions.gd
@@ -4,6 +4,13 @@ class_name UI_Actions
 
 @onready var _label = $Label
 @onready var _activity = $Activity
+@onready var _animation_player = $AnimationPlayer
+
+func showActions():
+	_animation_player.play("show")
+
+func hideActions():
+	_animation_player.play("hide")
 
 func setLabel(newLabel : String):
 	_label.text = newLabel
--- Scripts/Game/UI/PK.gd
+++ Scripts/Game/UI/PK.gd
@@ -2,19 +2,26 @@
 extends Sprite2D
 class_name UI_Pk
 
-@onready var team_1_name = $GridContainer/Team1Name
-@onready var team_2_name = $GridContainer/Team2Name
-@onready var team_1_score = $GridContainer/Team1Score
-@onready var team_2_score = $GridContainer/Team2Score
+@onready var _team_1_name = $GridContainer/Team1Name
+@onready var _team_2_name = $GridContainer/Team2Name
+@onready var _team_1_score = $GridContainer/Team1Score
+@onready var _team_2_score = $GridContainer/Team2Score
+@onready var _animation_player = $AnimationPlayer
+
+func showPK():
+	_animation_player.play("show")
+
+func hidePK():
+	_animation_player.play("hide")
 
 func setTeam1Name(newName : String):
-	team_1_name.text = newName
+	_team_1_name.text = newName
 	
 func setTeam2Name(newName : String):
-	team_2_name.text = newName
+	_team_2_name.text = newName
 	
 func setTeam1Score(newScore : int):
-	team_1_score.text = str(newScore)
+	_team_1_score.text = str(newScore)
 	
 func setTeam2Score(newScore : int):
-	team_2_score.text = str(newScore)
+	_team_2_score.text = str(newScore)
