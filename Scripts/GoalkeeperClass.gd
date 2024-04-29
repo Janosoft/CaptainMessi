80bf67fc00dfcae917c0f56b4e53d93d56aea213
Alejandro Lodes <janosoft@gmail.com>
2024-04-28 23:41:42 -0300

    privated scope

--- Scripts/GoalkeeperClass.gd
+++ Scripts/GoalkeeperClass.gd
@@ -4,6 +4,7 @@ class_name Goalkeeper
 
 @export var playerName : String
 @export var playerNumber : int
+@export var playerPosition : int
 @export var skinColor: Color = Color(0.996078, 0.8, 0.772549, 1)
 @export var hairColor: Color = Color(0.509804, 0.384314, 0.301961, 1)
 
@@ -11,7 +12,16 @@ class_name Goalkeeper
 @export var punchCount : int = 8
 @export var catchCount : int = 8
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var _animated_sprite_2d = $AnimatedSprite2D
+
+func showGoalKeeper(animation : String):
+	setAnimation(animation)
+	visible = true
+
+func hideGoalKeeper():
+	_animated_sprite_2d.stop()
+	visible = false
 
 func setAnimation(animation:String):
-	animated_sprite_2d.play(animation)
+	_animated_sprite_2d.stop()
+	_animated_sprite_2d.play(animation)
